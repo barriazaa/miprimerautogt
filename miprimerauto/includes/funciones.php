63010e2386223955116851b4b4799e240642d06b
barriazaa <barriazaa@miumg.edu.gt>
2026-04-14 16:59:25 -0600

    Correcciones
    Se corrigio una busqueda de archivos utilizando __DIR__ de PHP

--- miprimerauto/includes/funciones.php
+++ miprimerauto/includes/funciones.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'miprimerauto/includes/app.php';
+require __DIR__ . '/app.php';
 
 function incluirTemplate( string $nombre, bool $inicio = false ) {
     include TEMPLATES_URL . "/{$nombre}.php";
